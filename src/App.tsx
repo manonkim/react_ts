dff81009e0657427b9b0053dc3fa238566b324db
WY K <ablack1029@gmail.com>
2022-02-06 23:59:31 +0900

    Add : React.FC/props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [new Todo("learn React"), new Todo("learn Typescript")];
   return (
     <div className='App'>
-      <Todos />
+      <Todos items={todos} />
     </div>
   );
 }
